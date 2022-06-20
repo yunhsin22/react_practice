ea9b6345e329469485535bc4c02da9dc4a47789c
yunhsin22 <94583232+yunhsin22@users.noreply.github.com>
2022-06-20 23:18:20 +0800

    ex01

---
+++ learnReactJs_Basic_ex01.jsx
@@ -0,0 +1,17 @@
+<div id="root"></div>
+
+<script crossorigin src="https://unpkg.com/react@16/umd/react.production.min.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"></script>
+<!--Babel-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.1/babel.min.js"></script>
+
+<script type= "text/bael">
+
+let element = <h1>Hello, world</h1>
+
+ReactDom.render(
+    element,
+    document.getElementBtId('root')
+);
+
+</script>
